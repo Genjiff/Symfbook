53c117ba0c2854e0d775e38ffbb15792701057a2
Lucas Borges <jlucas19@hotmail.com>
2018-01-03 18:24:15 -0300

    #14 - Make internal pages of the application accessible only for logged in users

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -11,10 +11,19 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends Controller {
-    public function login(Request $request, AuthenticationUtils $authenticationUtils) {
+    public function indexPage (UserInterface $user = null) {
+        if($user == null) {
+            return $this->redirectToRoute('app_login');
+        } else {
+            return $this->redirectToRoute('app_profile');
+        }
+    }
+
+    public function login(AuthenticationUtils $authenticationUtils) {
         $error = $authenticationUtils->getLastAuthenticationError();
 
         $lastUsername = $authenticationUtils->getLastUsername();
